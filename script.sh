4282294c44b967af184c967dff6982763ca4efb1
Sagar Pramod Shimpi <sshimpi.sshimpi@hortonworks.com>
2017-09-12 23:51:29 +0530

    Added component timestamp calculatio script

--- script.sh
+++ script.sh
@@ -4,17 +4,40 @@
 # SCRIPT TO AUDIT UPGRADES
 #################################################################
 
-rm -fr /tmp/upgrade_audit
-mkdir /tmp/upgrade_audit
+tmp_dir="/tmp/upgrade_audit"
+rm -fr $tmp_dir
+mkdir $tmp_dir
 hostname=`hostname -f`
 port='8080'
 
 request_id=`curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades`
 
+comp_time_taken(){
+components="namenode datanode secondary standby zkfc resourcemanager nodemanager history mapreduce  zookeeper"
 
-curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades |grep "request_id" |tail -n 1 > /tmp/upgrade_audit/request_id
-request_id=`awk '{print $3}' /tmp/upgrade_audit/request_id`
-curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades/$request_id  |grep "group_id"  > /tmp/upgrade_audit/group_id
+for comp in `echo $components`
+do
+	stat=`grep -A10 -B 10 -i $comp $tmp_dir/status*`
+	val=`echo $?`
+	if [ $val == 1 ]
+	then
+		echo "$comp is not available in cluster" &>$tmp_dir/component_na
+	else
+		end_time=`echo $stat|grep end_time|cut -d':' -f2|cut -d',' -f1|sed 's/ //g'`
+		#echo $end_time
+		start_time=`echo $stat|grep start_time|cut -d':' -f3|cut -d',' -f1|sed 's/ //g'`
+		#echo $start_time
+		#echo "$comp $start_time $end_time" 
+		time_taken=`expr $end_time - $start_time`
+		echo "Time taken for upgrading ${comp} ==> ${time_taken}"
+	fi
+done
+}
+
+
+curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades |grep "request_id" |tail -n 1 > $tmp_dir/request_id
+request_id=`awk '{print $3}' $tmp_dir/request_id`
+curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades/$request_id  |grep "group_id"  > $tmp_dir/group_id
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #Get group_id for all task
@@ -23,13 +46,13 @@ curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$po
 for group_id in `awk '{print $3}' /tmp/group_id|sed 's/,/ /g'`
 do
 #echo $group_id
-	curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades/$request_id/upgrade_groups/$group_id > /tmp/upgrade_audit/stage_id_$group_id
+	curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET http://$hostname:$port/api/v1/clusters/hdptest/upgrades/$request_id/upgrade_groups/$group_id > $tmp_dir/stage_id_$group_id
 
-		for stage_id in `cat /tmp/upgrade_audit/stage_id_$group_id |grep href|sed '1d'|awk -F "\"" '{print $4}'`
+		for stage_id in `cat $tmp_dir/stage_id_$group_id |grep href|sed '1d'|awk -F "\"" '{print $4}'`
 		do
-			upgrade_item=`cat /tmp/upgrade_audit/stage_id_$group_id|grep $stage_id |awk -F"\"" '{print $4}'|awk -F "/" 'NF{print $NF}'`
+			upgrade_item=`cat $tmp_dir/stage_id_$group_id|grep $stage_id |awk -F"\"" '{print $4}'|awk -F "/" 'NF{print $NF}'`
 			#echo "++++++++++++++ $upgrade_item"
-			curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET $stage_id |grep -E -w "start_time|end_time|text|status" > /tmp/upgrade_audit/status_$upgrade_item
+			curl -s -u   admin:admin -H "X-Requested-By: ambari" -X GET $stage_id |grep -E -w "start_time|end_time|text|status" > $tmp_dir/status_$upgrade_item
 		done
 done
-
+comp_time_taken
